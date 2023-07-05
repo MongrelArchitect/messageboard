042421cc2a8f2798e621817d3da19f963866acee
Sean Eric Thomas <set@seanericthomas.com>
2023-07-05 16:45:53 -0700

    Submit and display messages

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,39 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+const messages = [
+  {
+    added: new Date(),
+    text: 'hi everyone',
+    user: 'some guy',
+  },
+  {
+    added: new Date(),
+    text: 'i like pizza',
+    user: 'PIZZAMAN',
+  },
+  {
+    added: new Date(),
+    text: 'pizza sucks',
+    user: 'nopizza',
+  },
+];
+
+router.get('/', (req, res) => {
+  res.render('index', { messages, title: 'Mini Messageboard' });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form');
+});
+
+router.post('/new', (req, res) => {
+  messages.push({
+    added: new Date(),
+    text: req.body.message,
+    user: req.body.user,
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
